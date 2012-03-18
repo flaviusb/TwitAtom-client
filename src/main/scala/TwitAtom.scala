50bd672f311be650a0d22e34a1bf04aa3f08f750
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2012-03-19 01:09:11 +1300

    Add in main activity stub.

---
+++ src/main/scala/TwitAtom.scala
@@ -0,0 +1,17 @@
+package net.flaviusb.twitatom
+
+import android.app.{Activity, ListActivity}
+import android.os.{Bundle, Message, Handler, Environment}
+import android.graphics.Canvas
+import android.widget._
+import android.content.{Context, Intent}
+import android.content.res.Resources
+
+import java.io._
+
+import android.util.Log
+
+
+class TwitAtom extends Activity {
+  
+}
