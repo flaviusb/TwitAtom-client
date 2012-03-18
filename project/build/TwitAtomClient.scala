b5678724c2017fbf9effa3ab2f7b6d854509ef03
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2012-03-18 23:18:30 +1300

    Add in start of sbt initialisation.

---
+++ project/build/TwitAtomClient.scala
@@ -0,0 +1,17 @@
+trait Defaults {
+  def androidPlatformName = "android-10"
+}
+class TwitAtomClient(info: ProjectInfo) extends ParentProject(info) {
+  override def shouldCheckOutputDirectories = false
+  override def updateAction = task { None }
+
+  lazy val main  = project(".", "TwitAtomClient", new MainProject(_))
+  //lazy val tests = project("tests",  "Templates-Tests", new TestProject(_), main)
+
+  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
+    val keyalias  = "TwitAtomClient"
+    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+  }
+
+  //class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults
+}
